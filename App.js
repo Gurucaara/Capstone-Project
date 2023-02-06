d588e47843e2b331ef8a96d32e3e788254f30d0a
Puru Rana <pururana@Purus-MacBook-Air.local>
2023-02-06 13:06:22 -0500

    Closes #1 ; Icon added for splash screen and splash screen created for the app

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
+import React, {useCallback, useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import Entypo from '@expo/vector-icons/Entypo';
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync(Entypo.font);
+        await new Promise(resolve => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}
+    onLayout={onLayoutRootView}>
+      <Text>Home Page</Text>
       <StatusBar style="auto" />
     </View>
   );
