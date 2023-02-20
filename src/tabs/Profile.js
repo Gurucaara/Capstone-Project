0d8cb91a444a92cc5f39e6dc1a640675ba3d952c
Mohammad Fayed <mohdfayed278@gmail.com>
2023-02-19 23:19:04 -0500

    Closes #8; Created user profile screen

--- src/tabs/Profile.js
+++ src/tabs/Profile.js
@@ -3,31 +3,35 @@ import { StyleSheet, View, Text } from "react-native";
 import { getData } from "../uitl";
 
 export default function () {
-  const [userData, setUserData] = useState({
-    userName: "",
-    userCountry: "",
-    userLanguage: "",
-    userAge: "",
-  });
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getData();
-      setUserData(data);
-    };
-    fetchData();
+    getData().then((res) => {
+      if (res) {
+        let response = JSON.parse(res);
+        setUserData(response);
+      }
+    });
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Name:</Text>
-      <Text style={styles.data}>{userData.userName}</Text>
-      <Text style={styles.label}>Country:</Text>
-      <Text style={styles.data}>{userData.userCountry}</Text>
-      <Text style={styles.label}>Language:</Text>
-      <Text style={styles.data}>{userData.userLanguage}</Text>
-      <Text style={styles.label}>Age:</Text>
-      <Text style={styles.data}>{userData.userAge}</Text>
+      <View style={styles.row}>
+        <Text style={styles.label}>Name:</Text>
+        <Text style={styles.data}>{userData.userName}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Country:</Text>
+        <Text style={styles.data}>{userData.userCountry}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Language:</Text>
+        <Text style={styles.data}>{userData.userLanguage}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Age:</Text>
+        <Text style={styles.data}>{userData.userAge}</Text>
+      </View>
     </View>
   );
 }
@@ -35,20 +39,29 @@ export default function () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 80,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "skyblue",
   },
-  label: {
-    fontSize: 16,
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
     marginTop: 20,
+  },
+  label: {
+    flex: 2,
+    fontSize: 16,
     color: "#FFF",
     fontWeight: "bold",
   },
   data: {
+    flex: 3,
     fontSize: 16,
     color: "#FFF",
     fontWeight: "bold",
+    textAlign: "right",
   },
 });
